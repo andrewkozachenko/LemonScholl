fe2e08415f294480bfc6f0a863a1d91340b01096
Andrew Kozachenko <andrew.kozachenko@gmail.com>
2017-11-24 01:07:19 +0200

    tic-tac-toe(krestiki noliki)

---
+++ src/school/lemon/finalMonth/XOField.java
@@ -0,0 +1,18 @@
+package school.lemon.finalMonth;
+
+public class XOField {
+    private static String winCombination = " " +
+            "123" + " " +
+            "456" + " " +
+            "789" + " " +
+            "147" + " " +
+            "258" + " " +
+            "369" + " " +
+            "159" + " " +
+            "357" +
+            " ";
+
+    public static String getWinCombination() {
+        return winCombination;
+    }
+}
